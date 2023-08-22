8f6dc51b4b8cef4876195817e178debe867823f2
Alexandr Rylkov <skynerstudent@gmail.com>
2023-08-22 14:23:48 +0300

    feat: change index.js

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import axios from 'axios'
 // import { join } from 'path'
 import { config } from 'dotenv'
 import express from 'express'
-import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 config()
 const app = express()
 
-const JOKE_API = 'https://v2.jokeapi.dev/joke/Programming?type=single'
 const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
 
 app.use(express.json())
@@ -18,54 +16,25 @@ app.use(
   })
 )
 
-const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID)
-await doc.useServiceAccountAuth({
-  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-})
-
 app.post('/new-message', async (req, res) => {
   const { message } = req.body
-
+  console.log(message);
   const messageText = message?.text?.toLowerCase()?.trim()
   const chatId = message?.chat?.id
+  console.log('chatId:' + chatId);
   if (!messageText || !chatId) {
     return res.sendStatus(400)
   }
 
-  // local json
-  // const dataFromJson = fs.readJSONSync(join(process.cwd(), 'todos.json'))
-
-  // google spreadsheet
-  await doc.loadInfo()
-  const sheet = doc.sheetsByIndex[0]
-  const rows = await sheet.getRows()
-  const dataFromSpreadsheet = rows.reduce((obj, row) => {
-    if (row.date) {
-      const todo = { text: row.text, done: row.done }
-      obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
-    }
-    return obj
-  }, {})
-
   let responseText = 'I have nothing to say.'
   // generate responseText
   if (messageText === 'joke') {
-    try {
-      const response = await axios(JOKE_API)
-      responseText = response.data.joke
-    } catch (e) {
-      console.log(e)
-      res.send(e)
-    }
-  } else if (/\d\d\.\d\d/.test(messageText)) {
-    // responseText = dataFromJson[messageText] || 'You have nothing to do on this day.'
-    responseText =
-      dataFromSpreadsheet[messageText] || 'You have nothing to do on this day.'
+    responseText = 'If Rusya Huy its normal'
   }
 
   // send response
   try {
+    console.log('ttttttttttttttttttt')
     await axios.post(TELEGRAM_URI, {
       chat_id: chatId,
       text: responseText
